17e1a1362c72f369c7dca44a14149b7ddf21a71b
E23CSEU1197 <e23cseu1197@bennett.edu.in>
2025-03-16 16:57:05 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Barter from "./pages/Barter";
+import Donations from "./pages/Donations";
+import Sell from "./pages/Sell";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/barter" element={<Barter />} />
+          <Route path="/donations" element={<Donations />} />
+          <Route path="/sell" element={<Sell />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
